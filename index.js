551ba74ea1748c18def3c81f3e48e91483ef4deb
Priyansh Singh <singhpriyansh2000@gmail.com>
2022-03-07 03:30:11 +0530

    completed the bored api project

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // "https://www.boredapi.com/api/activity"
+
+let buttonEl = document.getElementById("btn-el");
+let Activity = document.getElementById("activity");
+let headerEl = document.getElementById("heading");
+
+buttonEl.addEventListener("click", function () {
+  fetch("https://www.boredapi.com/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      Activity.textContent = data.activity;
+      headerEl.textContent = "Here's an activity :D";
+      document.body.classList.add("fun");
+    });
+});
